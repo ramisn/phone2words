6dbcf89b95effe59298f82ae94c8673b4c0bc479
Dmitriy Partsyrniy <dmitriy.partsyrniy@easternpeak.com>
2017-04-11 18:18:05 +0300

    First working version

--- phone2words.rb
+++ phone2words.rb
@@ -12,39 +12,41 @@ $number_map = {
 $dictionary = File.readlines('dictionary.txt').map{|w| w.strip}
 
 def word_exists(word)
-  $dictionary.index(word.upcase)
+  $dictionary.include?(word.upcase)
+end
+
+def phone2word(phone)
+  combinations = nil
+  phone.each_char do |num_char|
+    codes = $number_map[num_char.to_i]
+    raise "Invalid symbol '#{num_char}' in the phone" if codes.nil?
+    if combinations
+      combinations = combinations.product(codes)
+    else
+      combinations = codes
+    end
+  end
+  test_words = combinations.map { |e| e.flatten().join}
+  test_words.select! { |w| word_exists(w) }
+  test_words
 end
 
 #todo: make combinations of returned words
 def phone2words(initial_phone)
-  phone = initial_phone.clone
-  num = ''
+  last = initial_phone.clone
+  first = ''
   all_words = []
-  while phone!='' do
-    num = num + phone.slice!(0)
-    next if num.length < 3
-    combinations = nil
-    num.each_char do |num_char|
-      codes = $number_map[num_char.to_i]
-      raise "Invalid symbol '#{num_char}' in the phone" if codes.nil?
-      if combinations
-        combinations = combinations.product(codes)
-      else
-        combinations = codes
-      end
-    end
-    test_words = combinations.map { |e| e.flatten().join}
-    words = test_words.select! { |w| word_exists(w) }
-    if words.length>0
-      if phone.length>0
-        last_words = phone2words(phone)
-        return [words]+[last_words] if last_words.length>0
-      else
-        return [words]
-      end
+  while last.length>3 do
+    first = first + last.slice!(0)
+    next if first.length < 3
+    first_words = phone2word(first)
+    last_words = phone2word(last)
+    if first_words.length>0 && last_words.length>0
+      combinations = first_words.product(last_words)
+      all_words = all_words + combinations
     end
   end
-  []
+  all_words
 end
 
 p phone2words('2282668687')
