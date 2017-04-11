4f5f27451fc1bd0c0525b56e678e84f6ac8856c4
Dmitriy Partsyrniy <dmitriy.partsyrniy@easternpeak.com>
2017-04-11 16:17:24 +0300

    WIP: fix word_exists, calculate symbol combinations into word, etc

--- phone2words.rb
+++ phone2words.rb
@@ -1,39 +1,45 @@
 $number_map = {
-  2 => 'abc',
-  3 => 'def',
-  4 => 'ghi',
-  5 => 'jkl',
-  6 => 'mno',
-  7 => 'pqrs',
-  8 => 'tuv',
-  9 => 'wxyz'
+  2 => %w(a b c),
+  3 => %w(d e f),
+  4 => %w(g h i),
+  5 => %w(j k l),
+  6 => %w(m n o),
+  7 => %w(p q r s),
+  8 => %w(t u v),
+  9 => %w(w x y z)
 }
 
-$words = File.read('words.txt')
+$dictionary = File.readlines('words.txt').map{|w| w.strip}
 
-def wordexists(word)
-  $words =~ /#{word}/
+def word_exists(word)
+  $dictionary.index(word)
 end
 
-
-def phone2words(phone)
-  result = []
-  phone = phone.to_s
+# Return array of first possible words and rest of phone string
+def first_words(phone)
   num = ''
   while phone!='' do
     num = num + phone.slice!(0)
     next if num.length < 3
-    # test all words for s
     p num
+    combinations = nil
     num.each_char do |num_char|
-      code = $number_map[num_char.to_i]
-      raise "Invalid symbol '#{num_char}' in the phone" if code.nil?
-      code.each_char do |c|
+      codes = $number_map[num_char.to_i]
+      raise "Invalid symbol '#{num_char}' in the phone" if codes.nil?
+      if combinations
+        combinations = combinations.product(codes)
+      else
+        combinations = codes
       end
     end
+    test_words = combinations.map { |e| e.flatten().join}
+    words = test_words.select! { |w| word_exists(w) }
+    #p "words #{words}"
+    #break
+    return words, phone
   end
 end
 
-
-p phone2words(6686787825)
+p first_words('6686787825')
+#p phone2words(6686787825)
 
